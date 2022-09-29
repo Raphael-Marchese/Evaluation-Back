b18b5b94029301dbf79bc22897389318f189cb12
Raphael <marchese.raphael@gmail.com>
2022-09-29 17:57:02 +0200

    dernier push début reflexion stats dans le main

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\MovieRepository;
+use App\Repository\PersonRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,6 +14,14 @@ class MainController extends AbstractController
     public function index(): Response
     {
         return $this->render('main/index.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+    #[Route('/statistics', name: 'stats')]
+    public function getStats( MovieRepository $movieRepository, PersonRepository $personRepository): Response
+    {
+        $movies =$this->movieRepository-> findBy([], ['id' => 'ASC']);
+        return $this->render('main/statistics.html.twig', [
             'controller_name' => 'MainController',
         ]);
     }
