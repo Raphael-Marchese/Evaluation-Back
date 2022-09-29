6f2f3c27b13427c0cc2aec8a4c1ecb4221b24c0b
Raphael <marchese.raphael@gmail.com>
2022-09-29 17:22:10 +0200

    add movie fonctionality

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 use App\Entity\Movie;
+use App\Form\MovieType;
 use App\Repository\MovieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -23,6 +25,23 @@ class MovieController extends AbstractController
             dump(($movies));
             return $this->render('movie/list.html.twig', [
             'movies' => $movies
+        ]);
+    }
+    #[Route('/add', name: 'add')]
+    public function create(Request $request): Response
+    {
+        $newMovie = new Movie();
+        $form =$this->createForm(MovieType::class, $newMovie);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $this->movieRepository->save($newMovie, true);
+
+            $this->addFlash('success', 'Nouveau film ajouté avec succès!');
+            return $this->redirectToRoute('main_index');
+        }
+        return $this->render('movie/add.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 
--- src/Form/MovieType.php
+++ src/Form/MovieType.php
@@ -3,7 +3,9 @@
 namespace App\Form;
 
 use App\Entity\Movie;
+use phpDocumentor\Reflection\Types\Array_;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +14,19 @@ class MovieType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('release_at')
-            ->add('poster')
+            ->add('title', null, [
+                'label'=> 'Titre',
+            ])
+            ->add('release_at', null, [
+                'widget'=> 'single_text',
+                'label'=> 'Date de sortie',
+            ])
+            ->add('poster', null, [
+                'row_attr'=>['class'=>'form-element'],
+                'label'=> 'Nom de l\'image',
+            ])
             ->add('genre')
+
         ;
     }
 
